76ab988f277c8ec16e95d77a03270166bf93cff9
Marci260 <marcikanya@gmail.com>
2021-06-05 17:38:17 +0200

    Initial commit

---
+++ I-4-p/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace I_4_p
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
