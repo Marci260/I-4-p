7ffeeda635d1014aba9fc39b26cd48ab858282a8
Marci260 <55280098+Marci260@users.noreply.github.com>
2021-06-10 16:50:07 +0200

    Add files via upload

--- I-4-p/Program.cs
+++ I-4-p/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
 
 namespace I_4_p
 {
@@ -8,84 +11,25 @@ namespace I_4_p
     {
         static void Main(string[] args)
         {
-
-            string code = "alma";
-            string codekey = "acdf";
-            int[] temp = new int[code.Length];
-            var answer = "";
-            string abc = "abcdefghijklmnopqrstuvwxyz ";
-            //A B C D E F G H I J K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z  " "
-            //0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
-
-
-
-            //kodolas
-            for (int i = 0; i < code.Length; i++)
-            {
-
-                for (int j = 0; j < abc.Length; j++)
-                {
-                    if (code[i] == abc[j])
-                    {
-                        temp[i] += j;
-
-
-                    }
-
-
-                }
-
-            }
-            for (int i = 0; i < codekey.Length; i++)
-            {
-
-                for (int j = 0; j < abc.Length; j++)
-                {
-                    if (codekey[i] == abc[j])
-                    {
-                        temp[i] += j;
-
-                    }
-
-                }
-
-            }
-
-            ////////////////////idáig jó
-
-            for (int i = 0; i < temp.Length; i++)
-            {
-                for (int j = 0; j < abc.Length; j++)
-                {
-                    if (temp[i] > 26)
-                    {
-                        int number = temp[i] % 27;
-                        answer += abc[number];
-
-                    }
-                    else
-                    {
-                        answer += abc[temp[i]];
-
-                    }
-                    break;
-
-
-
-                }
-
-            }
+            Encrypt asd = new Encrypt("hello world", "abcdefgijkl");
+            string megoldas;
+            Encrypt secret = new Encrypt("hfnosebw vo", "abcdefgijkl");
+            string megoldas2; 
 
 
 
 
-            Console.WriteLine(code);
-            Console.WriteLine(codekey);
-            for (int i = 0; i < temp.Length; i++)
-            {
-                Console.WriteLine(temp[i]);
-            }
-            Console.WriteLine(answer);
+
+            megoldas = asd.coding();
+            megoldas2 = secret.decoding();
+          
+            Console.WriteLine(megoldas);
+            Console.WriteLine(megoldas2);
+          
+
+
+
+
             Console.ReadKey();
         }
     }
