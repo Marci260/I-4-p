cbf65c6e8190406b822ebebf26740d762259154d
Marci260 <55280098+Marci260@users.noreply.github.com>
2021-06-13 12:20:52 +0200

    Add files via upload

---
+++ I_4_P/I_4_P/Guess.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace I_4_P
+{
+    public class Guess
+    {
+        #region Variables
+        private string[] words;
+
+        public string[] Words
+        {
+            get { return words; }
+            set { }
+        }
+        private string abc;
+
+        public string Abc
+        {
+            get { return abc; }
+        }
+
+        #endregion
+
+        #region Constructors
+        public Guess(string[] aWords)
+        {
+            this.words = aWords;
+            this.abc = "abcdefghijklmnopqrstuvwxyz ";
+        }
+        #endregion
+
+        #region Methods
+        public string MakeAGuess(string message1, string message2)
+        {
+            //lopp (ha paratlan szam akkor message2, ha paros akkor message1)
+            //feltesszuk hogy mind a ket uzenet ugyan azzal a kulccsal keszult
+            string first_word = "early "; // megoldani hogy talalgatassal is menjen
+            string key = ""; // a kitalalt kulcsunk
+            int[] keyWordNumber = new int[message1.Length];
+            string word_fragment = "";
+            int currentMessageNumber = 1;
+            string currentmessage_first = "";
+            string currentmessage_second = "";
+
+
+            while (key.Length != message2.Length)
+            {
+
+
+                //eldontjuk hogy melyik uzenetet helyettesitjuk eppen
+                if (currentMessageNumber % 2 == 0)
+                {
+                    currentmessage_first = message2;
+                    currentmessage_second = message1;
+                }
+                else
+                {
+                    currentmessage_first = message1;
+                    currentmessage_second = message2;
+                }
+                //bele rakjuk egy tombbe az aktualisan kitalat szo karaktereinek hianyzo erteket
+                for (int i = 0; i < first_word.Length; i++)
+                {
+                    for (int j = 0; j < abc.Length; j++)
+                    {
+                        if (currentmessage_first[i] == abc[j] && i >= key.Length)
+                        {
+                            keyWordNumber[i] += j;
+                        }
+
+                    }
+                }
+
+                //kivonjuk belole az uzenet aktualis karaktereinek erteket
+                for (int i = 0; i < first_word.Length; i++)
+                {
+                    for (int j = 0; j < abc.Length; j++)
+                    {
+                        if (first_word[i] == abc[j] && i >= key.Length)
+                        {
+                            keyWordNumber[i] -= j;
+                        }
+                    }
+                }
+
+                //az igy kapott erteket bele iratjuk a kulcsunkba
+                for (int i = 0; i < first_word.Length; i++)
+                {
+                    for (int j = 0; j < abc.Length; j++)
+                    {
+                        if (i >= key.Length)
+                        {
+
+                            if (keyWordNumber[i] < 0)
+                            {
+                                int tempnum = keyWordNumber[i] + 27;
+                                key += abc[tempnum];
+                            }
+                            else
+                            {
+                                key += abc[keyWordNumber[i]];
+                            }
+                            break;
+                        }
+
+                    }
+
+                }
+
+
+
+
+                //kitisztítjuk a tombot
+                for (int i = 0; i < keyWordNumber.Length; i++)
+                {
+                    keyWordNumber[i] = 0;
+                }
+
+
+
+                //kulcs behelyettesitese a masik uzenetbe
+
+                for (int i = 0; i < first_word.Length; i++)
+                {
+                    for (int j = 0; j < abc.Length; j++)
+                    {
+                        if (currentmessage_second[i] == abc[j])
+                        {
+                            keyWordNumber[i] += j;
+                        }
+
+                    }
+                }
+
+                for (int i = 0; i < first_word.Length; i++)
+                {
+                    for (int j = 0; j < abc.Length; j++)
+                    {
+                        if (key[i] == abc[j])
+                        {
+                            keyWordNumber[i] -= j;
+                        }
+                    }
+                }
+
+                word_fragment = "";
+                //megkapjuk a szotoredeket
+                for (int i = 0; i < first_word.Length; i++)
+                {
+                    for (int j = 0; j < abc.Length; j++)
+                    {
+
+                        if (keyWordNumber[i] < 0)
+                        {
+                            int tempnum = keyWordNumber[i] + 27;
+                            word_fragment += abc[tempnum];
+                        }
+                        else
+                        {
+                            word_fragment += abc[keyWordNumber[i]];
+                        }
+                        break;
+                    }
+
+                }
+
+
+
+
+
+
+                // megkeressuk azokat a szavakat amiben benne van ez a toredek 
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (words[i].Contains(word_fragment))
+                    {
+                        first_word = words[i] += " ";
+                    }
+
+                }
+                currentMessageNumber++;
+
+            }
+
+            return key;
+        }
+        #endregion
+
+    }
+}
---
+++ I_4_P/I_4_P/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace I_4_P
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            #region Test
+            Encrypt asd = new Encrypt("hello world", "abcdefgijkl");
+            string megoldas;
+            Encrypt secret = new Encrypt("hfnosebw vo", "abcdefgijkl");
+            string megoldas2;
+            megoldas = asd.coding();
+            megoldas2 = secret.decoding();
+            Console.WriteLine(megoldas);
+            Console.WriteLine(megoldas2);
+
+            //beolvasas
+            #region read
+            string[] blockWords;
+            StreamReader Sr = new StreamReader("words.txt", Encoding.Default);
+            int db = 0;
+            while (!Sr.EndOfStream)
+            {
+                Sr.ReadLine();
+                db++;
+            }
+            Sr.Close();
+            StreamReader Read = new StreamReader("words.txt", Encoding.Default);
+            blockWords = new string[db];
+            int i = 0;
+            while (!Read.EndOfStream)
+            {
+                blockWords[i] += Read.ReadLine();
+                i++;
+
+            }
+            Read.Close();
+            #endregion
+
+            Guess myGuess = new Guess(blockWords);
+            string megoldas3;
+            megoldas3 = myGuess.MakeAGuess("kiuehzdamdkfugiatebfirta lo", "ibubyrklt vlezkxofjraqxs");
+            Console.WriteLine(megoldas3);
+            #endregion
+            */
+
+            Console.ReadKey();
+        }
+    }
+}
