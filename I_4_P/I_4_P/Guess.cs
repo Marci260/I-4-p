de434574d28391818072f32cb9026beb5c06e490
Marci260 <55280098+Marci260@users.noreply.github.com>
2021-07-06 22:49:41 +0200

    Add files via upload

--- I_4_P/I_4_P/Guess.cs
+++ I_4_P/I_4_P/Guess.cs
@@ -40,14 +40,26 @@ namespace I_4_P
             
             string first_word = "early "; 
             string key = ""; 
-            int[] keyWordNumber = new int[message1.Length];
             string word_fragment = "";
             int currentMessageNumber = 1;
             string currentmessage_first = "";
             string currentmessage_second = "";
+            int place = 0;
+            int messageLength;
+            #region Setup
+            if (message1.Length< message2.Length)
+            {
+                messageLength = message1.Length;
+            }
+            else
+            {
+                messageLength = message2.Length;
+            }
+            int[] keyWordNumber = new int[messageLength];
+            #endregion
 
-
-            while (key.Length != message2.Length)
+            #region ciklusok
+            while (key.Length != messageLength)
             {
                 
                 if (currentMessageNumber % 2 == 0)
@@ -60,119 +72,184 @@ namespace I_4_P
                     currentmessage_first = message1;
                     currentmessage_second = message2;
                 }
+
                
-                for (int i = 0; i < first_word.Length; i++)
+
+  
+
+                for (int i = 0; i < keyWordNumber.Length; i++)
                 {
                     for (int j = 0; j < abc.Length; j++)
                     {
-                        if (currentmessage_first[i] == abc[j] && i >= key.Length)
+                        if (i >= place && i<= first_word.Length-1)
                         {
-                            keyWordNumber[i] += j;
-                        }
-
+                            if (currentmessage_first[i] == abc[j])
+                            {
+                                keyWordNumber[i] += j;
+                            }
+                        }    
+                        
                     }
                 }
-
-               
-                for (int i = 0; i < first_word.Length; i++)
+                for (int i = 0; i < first_word.Length ; i++)
                 {
                     for (int j = 0; j < abc.Length; j++)
                     {
-                        if (first_word[i] == abc[j] && i >= key.Length)
+                        if (i>= place && i<= first_word.Length-1)
                         {
-                            keyWordNumber[i] -= j;
+
+
+                            if (first_word[i] == abc[j])
+                            {
+                                keyWordNumber[i] -= j;
+
+                            }
                         }
+                        
                     }
                 }
-
-               
+                
                 for (int i = 0; i < first_word.Length; i++)
                 {
                     for (int j = 0; j < abc.Length; j++)
                     {
                         if (i >= key.Length)
                         {
-
+                           
                             if (keyWordNumber[i] < 0)
                             {
                                 int tempnum = keyWordNumber[i] + 27;
                                 key += abc[tempnum];
+                               
+                            }
+                            else if (keyWordNumber[i] > 27)
+                            {
+                                int tempnum = keyWordNumber[i] % 27;
+                                key += abc[tempnum];
+                               
+                                
                             }
                             else
                             {
                                 key += abc[keyWordNumber[i]];
+                                
                             }
-                            break;
+                            
                         }
-
+                        break;
                     }
-
                 }
 
+                //
                 for (int i = 0; i < keyWordNumber.Length; i++)
                 {
                     keyWordNumber[i] = 0;
                 }
-
-                for (int i = 0; i < first_word.Length; i++)
+                //
+        
+                for (int i = 0; i < keyWordNumber.Length ; i++)
                 {
                     for (int j = 0; j < abc.Length; j++)
                     {
-                        if (currentmessage_second[i] == abc[j])
+                        if (i >= place && i <= key.Length)
                         {
-                            keyWordNumber[i] += j;
+                            if (currentmessage_second[i] == abc[j])
+                            {
+                                keyWordNumber[i] += j;
+                            }
                         }
+                        
 
                     }
                 }
-
-                for (int i = 0; i < first_word.Length; i++)
+              
+                for (int i = 0; i < currentmessage_second.Length ; i++)
                 {
                     for (int j = 0; j < abc.Length; j++)
                     {
-                        if (key[i] == abc[j])
+                        if (i >= place && i< key.Length)
                         {
-                            keyWordNumber[i] -= j;
+                            if (key[i] == abc[j])
+                            {
+                                keyWordNumber[i] -= j;
+                            }
+                            
                         }
+                        
                     }
+                    
                 }
 
-                word_fragment = "";
+              
                 
-                for (int i = 0; i < first_word.Length; i++)
+                word_fragment = "";
+
+                
+              
+                for (int i = 0; i <  keyWordNumber.Length; i++)
                 {
                     for (int j = 0; j < abc.Length; j++)
                     {
+                        if ( i >= place && i< key.Length)
+                        {
 
-                        if (keyWordNumber[i] < 0)
-                        {
-                            int tempnum = keyWordNumber[i] + 27;
-                            word_fragment += abc[tempnum];
+                            if (keyWordNumber[i] < 0)
+                            {
+                                int tempnum = keyWordNumber[i] + 27;
+                                word_fragment += abc[tempnum];
+                            }
+                            else if (keyWordNumber[i] > 27)
+                            {
+                                int tempnum = keyWordNumber[i] % 27;
+                                word_fragment += abc[tempnum];
+                            }
+                            else
+                            {
+                                word_fragment += abc[keyWordNumber[i]];
+                            }
+                            break;
                         }
-                        else
-                        {
-                            word_fragment += abc[keyWordNumber[i]];
-                        }
-                        break;
                     }
-
+                    
                 }
+               
+
+                place = key.Length;
+
+                ;
+                first_word = "";
 
                 for (int i = 0; i < words.Length; i++)
                 {
-                    if (words[i].Contains(word_fragment))
+                    if (words[i].Contains(word_fragment) )
                     {
-                        first_word = words[i] += " ";
+                        first_word = words[i];
+
+                        if (first_word[first_word.Length-1] != ' ')
+                        {
+                            first_word += ' ';
+                        }
+                        break;
                     }
-
+                    
+                    
                 }
-                currentMessageNumber++;
+                for (int i = 0; i < keyWordNumber.Length; i++)
+                {
+                    keyWordNumber[i] = 0;
+                }
+               
+                
+               
 
+                currentMessageNumber++;
+                
             }
 
+            #endregion
             return key;
         }
-        #endregion
+            #endregion
 
     }
 }
