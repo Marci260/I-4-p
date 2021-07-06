f4c9044f8eac3a45f4be3965a1766490ea30cd06
Marci260 <55280098+Marci260@users.noreply.github.com>
2021-07-06 22:54:00 +0200

    Add files via upload

--- I_4_P/I_4_P/Program.cs
+++ I_4_P/I_4_P/Program.cs
@@ -12,15 +12,24 @@ namespace I_4_P
         static void Main(string[] args)
         {
             /*
+            
             #region Test
-            Encrypt asd = new Encrypt("hello world", "abcdefgijkl");
+            Encrypt asd = new Encrypt("early bird catches the worm", "oirkd uertma dtuafkbiweymud");
             string megoldas;
-            Encrypt secret = new Encrypt("hfnosebw vo", "abcdefgijkl");
+            Encrypt secret = new Encrypt("sihvazvmhwlc wvaexjup dt kp", "oirkd uertma dtuafkbiweymud");
             string megoldas2;
+            Encrypt asd2 = new Encrypt("curiosity killed the cat", "oirkd uertma dtuafkbiwey");
+            string megoldas4;
+            Encrypt secret2 = new Encrypt("qbhsrrbxoswikoxx yrfhyeq", "oirkd uertma dtuafkbiweymud");
+            string megoldas5;
             megoldas = asd.coding();
             megoldas2 = secret.decoding();
+            megoldas4 = asd2.coding();
+            megoldas5 = secret2.decoding();
             Console.WriteLine(megoldas);
             Console.WriteLine(megoldas2);
+            Console.WriteLine(megoldas4);
+            Console.WriteLine(megoldas5);
 
             //beolvasas
             #region read
@@ -47,11 +56,24 @@ namespace I_4_P
 
             Guess myGuess = new Guess(blockWords);
             string megoldas3;
-            megoldas3 = myGuess.MakeAGuess("kiuehzdamdkfugiatebfirta lo", "ibubyrklt vlezkxofjraqxs");
-            Console.WriteLine(megoldas3);
-            #endregion
-            */
+            megoldas3 = myGuess.MakeAGuess("sihvazvmhwlc wvaexjup dt kp", "qbhsrrbxoswikoxx yrfhyeq");
 
+            Console.WriteLine("\n");
+            Console.WriteLine("megoldas: oirkd uertma dtuafkbiweymud") ;
+            Console.WriteLine("\n");
+
+            Console.WriteLine(megoldas3);
+
+
+            Encrypt secret3 = new Encrypt("sihvazvmhwlc wvaexjup dt kp","oirkd uertmjapwyfzkgqzer");
+            string megoldas6 = secret3.decoding();
+            Encrypt secret4 = new Encrypt("qbhsrrbxoswikoxx yrfhyeq", "oirkd uertmjapwyfzkgqzer");
+            string megoldas7 = secret4.decoding();
+            Console.WriteLine(megoldas6);
+            Console.WriteLine(megoldas7);
+            #endregion
+            
+            */
             Console.ReadKey();
         }
     }
